c2c98e64c4588b2c0fa1db6472321379280963aa
Samuel Brewer <sam.brewer13@yahoo.com>
2024-02-16 16:49:07 -0700

    function overload

--- src/app.ts
+++ src/app.ts
@@ -21,12 +21,16 @@ type Numeric = number | boolean;
 
 type Universal = Combinable & Numeric;
 
+function add(a: number, b: number): number;
+function add(a: string, b: string): string;
 function add(a: Combinable, b: Combinable) {
     if (typeof a === 'string' || typeof b === 'string') {
         return a.toString() + b.toString();
     }
     return a + b;
 }
+
+const result = add('Samuel', 'Brewer');
 
 type UnknownEmplyee = Employee | Admin;
 
@@ -115,3 +119,8 @@ interface ErrorContainer {
     // { email: 'Not a valid email, username: 'Must start with a character' }
     [prop: string]: string;
 }
+
+const errorBag: ErrorContainer = {
+    email: 'Not a valid email',
+    username: 'Must start with a capital character',
+};
